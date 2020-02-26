c08919888b7fff5a8682d7c5f3afb785a65a61a4
KOMINIARZ Anaïs <kominiarz.anais@gmail.com>
2020-02-26 17:48:49 +0100

    "7e commit fin TP3"

--- premier-projet-maven/src/main/java/dev/App.java
+++ premier-projet-maven/src/main/java/dev/App.java
@@ -4,14 +4,21 @@ package dev;
  * Hello world!
  *
  */
+import java.util.ResourceBundle;
 import com.github.lalyos.jfiglet.FigletFont;
 
 public class App 
 {
     public static void main( String[] args )
     {
-    	String asciiArt = FigletFont.convertOneLine("hello");
-        System.out.println(asciiArt);
+    	String titre = ResourceBundle.getBundle("application").getString("titre");
+    	String asciiArt = FigletFont.convertOneLine(titre);
+    	System.out.println(asciiArt);
+    	
+    	// affichage de l'environnement         
+    	String environnement = ResourceBundle.getBundle("application").getString( "environnement");
+    	System.out.println("Environnement : " + environnement);
+
     }
     
     
