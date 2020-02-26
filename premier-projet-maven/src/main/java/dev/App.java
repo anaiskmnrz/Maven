25b9df8b1a759e68e633be9b41c4f746025b3da8
KOMINIARZ Anaïs <kominiarz.anais@gmail.com>
2020-02-26 15:28:15 +0100

    "4e commit (dernier du TP1)"

--- premier-projet-maven/src/main/java/dev/App.java
+++ premier-projet-maven/src/main/java/dev/App.java
@@ -4,11 +4,15 @@ package dev;
  * Hello world!
  *
  */
+import com.github.lalyos.jfiglet.FigletFont;
 
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	String asciiArt = FigletFont.convertOneLine("hello");
+        System.out.println(asciiArt);
     }
+    
+    
 }
